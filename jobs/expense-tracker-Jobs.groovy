383198f068cef80f2955b504cee8556f3700bcad
firmsoil <firmsoil@gmail.com>
2016-01-10 11:37:34 -0800

    Create expense-tracker-Jobs.groovy

---
+++ jobs/expense-tracker-Jobs.groovy
@@ -0,0 +1,26 @@
+/**
+ * Created by isc_2246 on 1/10/16.
+ */
+
+String basePath = 'expense-tracker'
+String repo = 'sheehan/grails-example'
+
+folder(basePath) {
+    description 'This example shows basic folder/job creation.'
+}
+
+
+job("$basePath/expense-tracker-base-build") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        grails {
+            useWrapper true
+            targets(['test-app', 'war'])
+        }
+    }
+}

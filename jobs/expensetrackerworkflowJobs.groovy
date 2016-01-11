cf2e75c019af6324c220e52b0053ce8d240cadc7
firmsoil <firmsoil@gmail.com>
2016-01-10 19:57:49 -0800

    Update expensetrackerworkflowJobs.groovy

--- jobs/expensetrackerworkflowJobs.groovy
+++ jobs/expensetrackerworkflowJobs.groovy
@@ -1,4 +1,8 @@
 workflowJob('expensetrackerworkflow') {
+    
+        triggers {
+        scm 'H/5 * * * *'
+    }
     definition {
         cps {
             script(readFileFromWorkspace('resources/expense-tracker-wf.groovy'))
